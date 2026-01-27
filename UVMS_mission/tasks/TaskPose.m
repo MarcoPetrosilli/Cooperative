8cc0615953fc3a8654ec15ca96b9333dbf12f3b2
giorgio.marmolino <giorgio.marmolino@gmail.com>
2026-01-27 12:18:54 +0100

    Update Unified Action

--- UVMS_mission/tasks/TaskPose.m
+++ UVMS_mission/tasks/TaskPose.m
@@ -6,10 +6,10 @@ classdef TaskPose < Task
     methods
         function updateReference(obj, robot)
             [ang, lin] = CartError(robot.wTgv , robot.wTv);
-            obj.xdotbar = 1.2 * [ang; lin];
-            obj.xdotbar(1:3) = Saturate(obj.xdotbar(1:3), 0.4);
+            obj.xdotbar = 0.8 * [ang; lin];
+            obj.xdotbar(1:3) = Saturate(obj.xdotbar(1:3), 0.3);
             % obj.xdotbar(3) = 0.0;
-            obj.xdotbar(4:6) = Saturate(obj.xdotbar(4:6), 0.4);
+            obj.xdotbar(4:6) = Saturate(obj.xdotbar(4:6), 0.3);
         end
         function updateJacobian(obj, robot)
             obj.J = [zeros(3,7) zeros(3,3) robot.wTv(1:3,1:3);
